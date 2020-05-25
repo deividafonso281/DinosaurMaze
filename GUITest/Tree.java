176236226fd29dbe9ce5647f153a44f410089539
David <david.AP.999@gmail.com>
2020-05-25 19:13:48 -0300

    I won't let these little things slip out of my mouth. But if it's true, it's you, it's you, they add up to. I'm in love with you and all these little things.

--- GUITest/Tree.java
+++ GUITest/Tree.java
@@ -1,12 +1,17 @@
 import java.awt.Graphics;
 import java.awt.Color;
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
+import java.awt.image.BufferedImage;
 
 public class Tree  {
 
-        int size = 32;
+        int size = 36;
         public int posx;
         public int posy;
 	public boolean isObstacle;
+	private BufferedImage img = null;
 
         public Tree (int i, int j) {
                  posx = i;
@@ -16,9 +21,14 @@ public class Tree  {
 
         public void draw (Graphics g) {
 		if (isObstacle == true) {
-                	g.setColor(Color.RED);
-                	g.fillOval((posx-1)*36+2,80+(posy-1)*36+2,size,size);
-        	}
+                	/*g.setColor(Color.RED);
+                	g.fillOval((posx-1)*36+2,80+(posy-1)*36+2,size,size);*/
+        		try {
+				img = ImageIO.read(new File("arvred.png"));
+			} catch (IOException e) {
+			}
+			g.drawImage(img, (posx-1)*36,80+(posy-1)*36,null);
+		}
 	}
 
 	public void setObstacle () {
