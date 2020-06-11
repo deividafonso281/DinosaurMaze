44a21ef585d190342851b7e459be15547f19db36
David <david.AP.999@gmail.com>
2020-06-10 23:16:37 -0300

    As luz tao piscando o jukebox tocando, com bruno e barreto o pau ta quebrando.

--- GUITest/LeaderBoard.java
+++ GUITest/LeaderBoard.java
@@ -4,6 +4,12 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JLabel;
 import java.awt.CardLayout;
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
+import java.awt.image.BufferedImage;
+import java.awt.Graphics;
+import java.awt.Color;
 
 public class LeaderBoard extends JPanel {
 
@@ -16,9 +22,11 @@ public class LeaderBoard extends JPanel {
 	private JButton goBack;
 	private JLabel title;
 	private JLabel[] scores = new JLabel[15];
+	private BufferedImage img;
 
 	public LeaderBoard () {
 		setLayout(null);
+		setImage();
 		title = new JLabel("Best Players");
 		title.setSize(620,40);
 		title.setLocation(50,0);
@@ -64,4 +72,21 @@ public class LeaderBoard extends JPanel {
 	public void saveScores() {
 		scoreListWritter.saveScoreList(scoreList);
 	}
+
+	private void setImage() {
+                try {
+                        img = ImageIO.read(new File("menu6.png"));
+                } catch (IOException e) {
+                }
+        }
+
+        @Override
+        protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                g.drawImage(img,0,0,null);
+		g.setColor(Color.WHITE);
+		for (int i=0;i<15;i++) {
+                        g.fillRect(50,40+i*40,620,30);
+                }
+        }
 }
